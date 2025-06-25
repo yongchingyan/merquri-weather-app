f7fd3f471507632175e4852d4cf07cc032879e82
Yong Ching Yan <yongchingyan@gmail.com>
2025-06-25 13:26:14 +0800

    clean up console.log

--- src/components/weather-app.tsx
+++ src/components/weather-app.tsx
@@ -14,7 +14,7 @@ export default function WeatherApp () {
     const [weather, setWeather] = useState<typeof MOCK_COMBINED_WEATHER_API>(MOCK_DATA);
 
     const onSubmit = async (city: string, country: string) => {
-        console.log(`submit ${city}, ${country}`)
+        // console.log(`Searching for... ${city}, ${country}`)
         if (!city || !country){
             setError("Please fill in both fields.");
             return false;
@@ -28,8 +28,7 @@ export default function WeatherApp () {
             }
         })
         .then(function (response) {
-            // console.log(JSON.stringify(response.data));
-            console.log(response.data);
+            // console.log(response.data);
             if (response.data.length>0){
 
                 return axios.get("https://api.openweathermap.org/data/3.0/onecall", {
@@ -42,16 +41,12 @@ export default function WeatherApp () {
                     }
                 })
                 .then(function (response1) {
-                    // console.log(JSON.stringify(response1.data));
-                    console.log(response1.data);
+                    // console.log(response1.data);
                     if (response1.data){
                         const timestamp = UTCtoDateTime(response1.data.current.dt);
-                        // const timestamp = new Date(response1.data.current.dt * 1000).toLocaleString('en-SG', {year: 'numeric', month: '2-digit', day: '2-digit',  hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true }).replace(/\//g, '-').replace(/,/g, '');
-                        // new Date().toLocaleString('en-SG', {year: 'numeric', month: '2-digit', day: '2-digit',  hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true }).replace(/\//g, '-')
-                        console.log(timestamp);
 
                         setWeather({city: formatCity(city), country: formatCountry(country), timestamp, ...response1.data})
-                        console.log(`saving weather.... `, {city: formatCity(city), country: formatCountry(country), ...response1.data})
+                        // console.log(`Saving weather.... `, {city: formatCity(city), country: formatCountry(country), ...response1.data})
                         setHistory([
                             [
                                 formatCity(city), 
@@ -68,7 +63,6 @@ export default function WeatherApp () {
                     }
                 })
                 .catch(function (error) {
-                    console.log(error.message);
                     setError(error.message);
                     return false;
                 })
@@ -79,14 +73,12 @@ export default function WeatherApp () {
             }
         })
         .catch(function (error) {
-            console.log(error);
-            setError(error);
+            setError(error.message);
             return false;
         })
     }
 
     const handleDelete = (index: number) => () => {
-        console.log(`handleDelete, ${index}`) 
         const newHistory = [...history].filter((_,key) => key!=index)
         setHistory(newHistory);
     }
